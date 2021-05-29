fe1bc0881369fe70e3271a4e051281c3523cd6a5
progcsharp <fonror@mail.ru>
2021-05-29 19:28:42 +0300

    fix admin name

--- back/myproject/myproject/apps/Project/models.py
+++ back/myproject/myproject/apps/Project/models.py
@@ -9,3 +9,9 @@ class Content(models.Model):
     text = models.TextField('текст')
     image = models.FileField('картинка',upload_to = 'media/image/')    
     icon = models.FileField('иконкa',upload_to = 'media/image/')
+    def __str__(self):
+        return 'Контент'
+    
+    class Meta:
+        verbose_name = 'контент'
+        verbose_name_plural = 'контент'
