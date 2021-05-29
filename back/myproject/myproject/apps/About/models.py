3d84ad1f8e512d63982c4f0e94192aa931d528f3
progcsharp <fonror@mail.ru>
2021-05-29 19:12:31 +0300

    редактор текста в админке

--- back/myproject/myproject/apps/About/models.py
+++ back/myproject/myproject/apps/About/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Meta:
-    verbose_name = 'Страница о себе'
+    verbose_name = 'О себе'
+    verbose_name_plural = 'О себе'
+    
     
 
 class Content(models.Model):
     text = models.TextField('текст')
     image = models.FileField('картинка',upload_to = 'media/image/')    
     icon = models.FileField('иконкa',upload_to = 'media/image/')
+    def __str__(self):
+        return 'Контент'
+    
+    class Meta:
+        verbose_name = 'контент'
+        verbose_name_plural = 'контент'
