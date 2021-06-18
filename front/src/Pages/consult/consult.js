137ce64e89b7789a26946fefc57fd4851780be30
frity <nikonovfedor36936@gmail.com>
2021-06-18 17:58:38 +0300

    front: consult page tpl

---
+++ front/src/Pages/consult/consult.js
@@ -0,0 +1,10 @@
+import './../../bundle';
+
+// Code libs and plugins
+import { globalEventone } from '../../Plugins/eventone.js';
+
+globalEventone();
+
+import './../../Blocks/section/section';
+import './../../Blocks/contacts/contacts';
+import './../../Blocks/recommended-products/recommended-products';
