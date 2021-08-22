ad8055ab878a89a0cd9c3e5c009af63a0e077559
frity <nikonovfedor36936@gmail.com>
2021-08-22 10:22:16 +0300

    front: transp logo, select autoclose, buy page adaptive

--- front/src/Basic/select/select.js
+++ front/src/Basic/select/select.js
@@ -4,15 +4,36 @@ $(document).ready(() => {
    const pref = '.select'; // prefix for current folder
    
    $(`${pref}__basis`).click(action('click: select__basis', e => {
-      $(e.target).parents(pref).find(`${pref}__dropdown`).toggleClass('dn');
+      $(e.target)
+         .parents(pref)
+         .find(`${pref}__dropdown`)
+         .toggleClass('dn');
    }));
+   
    $(`${pref}__option`).click(action('click: select__option', e => {
       const $select = $(e.target).parents(pref);
-      $select.find(`${pref}__title`).text(e.target.textContent);
+      $select
+         .find(`${pref}__title`)
+         .text(e.target.textContent);
+
       const classSelected = `${pref}__option_selected`.slice(1);
-      $select.find('.'+classSelected).removeClass(classSelected);
+
+      $select
+         .find('.'+classSelected)
+         .removeClass(classSelected);
+
       e.target.classList.add(classSelected);
+      
       $(`${pref}__basis`, $select).trigger('click');
       action('change: select')(e, $(e.target), $select);
+   }));
+
+   $(`${pref}`).on('mouseleave', action('mouseleave: select', e => {
+      e.currentTarget.leaveTimeout = setTimeout(() => {
+         $(`${pref}__dropdown`, e.currentTarget).addClass('dn');
+      }, 1500);
+   }));
+   $(`${pref}`).on('mouseenter', action('mouseenter: select', e => {
+      clearTimeout(e.currentTarget.leaveTimeout);
    }));
 });
