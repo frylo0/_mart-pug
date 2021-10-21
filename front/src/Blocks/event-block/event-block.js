4ebab2e89d1b47b80a49c2fd003d800d68102fb7
frity <nikonovfedor36936@gmail.com>
2021-10-22 02:23:46 +0300

    front: event page adaptive

---
+++ front/src/Blocks/event-block/event-block.js
@@ -0,0 +1,5 @@
+//$(document).ready(() => {
+//   const pref = '.event-block'; // prefix for current folder
+//   
+//   $(`${pref}`)
+//});
---
+++ front/src/Pages/event/event.js
@@ -0,0 +1,9 @@
+import './../../bundle';
+
+// Code libs and plugins
+import { globalEventone } from '../../Plugins/eventone.js';
+
+globalEventone();
+
+import './../../Blocks/contacts/contacts';
+import './../../Blocks/event-block/event-block';
