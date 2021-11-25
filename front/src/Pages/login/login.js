bb7b7ebebfbc9818d03248c733fb1acc4d42366a
frity <nikonovfedor36936@gmail.com>
2021-11-25 21:31:59 +0300

    front: my-materials, login/register, sales - adaptive

---
+++ front/src/Pages/login/login.js
@@ -0,0 +1,51 @@
+import './../../bundle';
+
+// Code libs and plugins
+import { globalEventone } from '../../Plugins/eventone.js';
+
+globalEventone();
+
+$('.button_input-controls-eye').on('click', e => {
+   const $input = $('.button_input-controls-eye').parent().prev('input');
+   if ($input.attr('type') == 'password') $input.attr('type', 'text')
+   else $input.attr('type', 'password');
+});
+
+function isPasswordsEqual($inputsPassword) {
+   const values = $inputsPassword.map((i, input) => input.value);
+   return values[0] == values[1];
+}
+
+const $inputsPassword = $('.login_register-input .input-controls__input[type="password"]');
+
+const $messagePasswordsCorrect = $('.office_password-message [role="correct"]');
+const $messagePasswordsError = $('.office_password-message [role="error"]');
+const $messagePasswordsFree = $('.office_password-message [role="free"]');
+
+const $messagesPasswords = [$messagePasswordsCorrect, $messagePasswordsError, $messagePasswordsFree];
+$messagesPasswords.slideUpAll = function () {
+   this.forEach($message => $message.slideUp(0));
+};
+
+const $changePasswordButton = $('.login_register-button');
+$changePasswordButton.enable = function () { this.removeAttr('disabled'); };
+$changePasswordButton.disable = function () { this.attr('disabled', 'disabled'); };
+
+const passwordTimediffer = new Timediffer(500);
+$inputsPassword.on('keyup', e => $changePasswordButton.disable() || passwordTimediffer.ifReached(() => {
+   if (e.target.value == '') {
+      $messagesPasswords.slideUpAll();
+      $messagePasswordsFree.slideDown();
+   } else if (!isPasswordsEqual($inputsPassword)) {
+      $messagesPasswords.slideUpAll();
+      $messagePasswordsError.slideDown();
+   } else {
+      $messagesPasswords.slideUpAll();
+      $messagePasswordsCorrect.slideDown();
+      $changePasswordButton.enable();
+   }
+}));
+
+import './../../Blocks/product/product';
+import './../../Blocks/product-office/product-office';
+import './../../Blocks/recommended-products/recommended-products';
---
+++ front/src/Pages/my-materials/my-materials.js
@@ -0,0 +1,10 @@
+import './../../bundle';
+
+// Code libs and plugins
+import { globalEventone } from '../../Plugins/eventone.js';
+
+globalEventone();
+
+import './../../Blocks/product/product';
+import './../../Blocks/product-office/product-office';
+import './../../Blocks/recommended-products/recommended-products';
